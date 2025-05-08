811d0104530083a2ff31a917dfd7217abbc8d26c
ahah2025 <adghty61>
2025-05-08 17:03:57 +0900

    2025-05-08

---
+++ src/com/javaex/ex01/Customer.java
@@ -0,0 +1,50 @@
+//.자식 클래스
+package com.javaex.ex01;
+
+public class Customer extends Person{
+	
+	//필드
+	private int cNo; //고객점수
+	private int point; // 포인트점수
+	
+	//생성자
+	public Customer() {
+		super("정우성","010-1111-2222");
+	}
+	
+	public Customer(int cNo, int point) {
+		this.cNo = cNo;
+		this.point = point;
+	}
+
+	public Customer(String name, String hp,int cNo, int point) {
+		super(name, hp);
+		this.cNo = cNo;
+		this.point = point;
+	}
+
+	//메소드gs
+	public int getcNo() {
+		return cNo;
+	}
+
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	//메소드 일반
+	public void showInfo() {
+		System.out.print("#이름:"+super.getName()+","+"\t");
+		System.out.print("#핸드폰:"+super.getHp()+","+"\t");
+		System.out.print("#고객점수:"+this.cNo+","+"\t");
+		System.out.print("#포인트점수:"+this.point);
+	}
+}
---
+++ src/com/javaex/ex01/Person.java
@@ -0,0 +1,43 @@
+//부모 클래스
+package com.javaex.ex01;
+
+public class Person {
+	
+	//필드
+	private String name; //이름
+	private String hp;  //핸드폰
+	
+	//생성자
+	public Person() {
+	}
+
+	public Person(String name, String hp) {
+		this.name = name;
+		this.hp = hp;
+	}
+
+	//메소드gs
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	//메소드일반
+	public void showInfo() {
+		System.out.print("#이름:"+this.getName()+","+"\t");
+		System.out.println("#핸드폰:"+this.getHp());
+	}
+	
+	
+}
